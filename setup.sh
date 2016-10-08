60b64fe2ce2bcaa2c8b74f2f098fa6efbf4f4ed5
Brandon <willjasen@gmail.com>
2016-10-08 15:34:39 -0400

    update bittorrent sync to resilio sync

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-# Install and run BitTorrent Sync
-mkdir ~/.btsync
-curl -o ~/.btsync/BitTorrent-Sync_arm.tar.gz https://download-cdn.getsync.com/stable/linux-arm/BitTorrent-Sync_arm.tar.gz
-tar xfv ~/.btsync/BitTorrent-Sync_arm.tar.gz -C ~/.btsync
-sudo cp btsync /etc/init.d
-cp btsync.conf ~/.btsync
-sudo chmod 755 /etc/init.d/btsync
-sudo update-rc.d btsync defaults
-sudo service btsync start
+# Install and run Resilio Sync
+mkdir ~/.rslsync
+curl -o ~/.rslsync/resilio-sync_arm.tar.gz https://download-cdn.resilio.com/stable/linux-arm/resilio-sync_arm.tar.gz
+tar xfv ~/.rslsync/resilio-sync_arm.tar.gz -C ~/.rslsync
+sudo cp rslsync /etc/init.d
+cp rslsync.conf ~/.rslsync
+sudo chmod 755 /etc/init.d/rslsync
+sudo update-rc.d rslsync defaults
+sudo service rslsync start
