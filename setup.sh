ff01e63b86f5301ca6c210affc7a2b7a23c0be50
Brandon <willjasen@gmail.com>
2017-11-19 22:24:00 -0500

    formatting and future planning

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
-# Install and run Resilio Sync
+# Download and install
 mkdir ~/.rslsync
 curl -o ~/.rslsync/resilio-sync_arm.tar.gz https://download-cdn.resilio.com/stable/linux-arm/resilio-sync_arm.tar.gz
 tar xfv ~/.rslsync/resilio-sync_arm.tar.gz -C ~/.rslsync
 sudo cp rslsync /etc/init.d
 cp rslsync.conf ~/.rslsync
 sudo chmod 755 /etc/init.d/rslsync
+
+# Update the config file
+#   --sed inline commands here--
+
+# Run as a service
 sudo update-rc.d rslsync defaults
 sudo service rslsync start
 
